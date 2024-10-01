2de7aec86690d11220364b5cd649edf7e037a524
Matas5 <matas.andraitis@gmail.com>
2024-10-01 13:16:47 +0300

    Added "generate-problem" method to GameController.cs and changed the Program.cs to include CORS

--- Program.cs
+++ Program.cs
@@ -4,13 +4,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 //defined in util/DependencyInjections.cs
 builder.Services.AddCustomServices();
 
+// Configure CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000") 
+                  .AllowAnyMethod()
+                  .AllowAnyHeader();
+        });
+});
 
 var app = builder.Build();
 
@@ -21,6 +30,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowFrontend");
 
 app.UseAuthorization();
 
